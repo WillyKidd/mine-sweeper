143f83f696da5219af56fecd4cfee32c95173af7
weili@manjaro <a320_271N@outlook.com>
2022-06-27 16:26:40 +0800

    modified header

--- miner-front/src/App.vue
+++ miner-front/src/App.vue
@@ -1,14 +1,35 @@
+<style>
+  @import './assets/css/styles.css';
+</style>
+
 <template>
   <div>
-    <b-navbar type="dark" variant="dark">
-      <div class="container">
-        <b-navbar-nav>
-          <b-nav-item @click="mmm()" href="#">
-            Avoid the bombs!
-          </b-nav-item>
-        </b-navbar-nav>
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Weili's Home Page</title>
+    </head>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Weili's Personal Website</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">My Blog</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="flight.html">My Past Flights</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="https://willykidd.github.io/mine-sweeper/">Game</a> </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Dropdown </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a> 
+            </div>
+          </li>
+        </ul>
       </div>
-    </b-navbar>
+    </nav>
     <div>
       <GameBoard></GameBoard>
     </div>    
